38e03c948140f8eb4801263cae8f1f6beac4d205
Jared <jaredcmurphy@gmail.com>
2016-04-01 10:42:17 -0400

    adds nested components rendered on view

--- app/index.js
+++ app/index.js
@@ -1,17 +1,53 @@
+var USER_DATA = {
+  name: 'Jared Murphy',
+  username: 'jaredmurphy',
+  image: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAZjAAAAJGFhZTA0Y2Y4LTQ3YTktNDljYi1hM2I3LWU3MjlhZjYwMDE0MQ.jpg'
+}
+
+
 var React = require('react');
+
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
+var ProfilePic = React.createClass({
   render: function() {
-    return (
-      <div> Hello World </div>
-      )
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
   }
 });
 
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
 
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+    <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+    <div>
+      <ProfilePic imageUrl={this.props.user.image} />
+      <ProfileName name={this.props.user.name} />
+      <ProfileLink username={this.props.user.username} />
+    </div>
+    )
+  }
+});
 
 ReactDOM.render(
-  <Hello/>,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );

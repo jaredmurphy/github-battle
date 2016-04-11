ff76ebb6b411ff7954514cb110b90af09891884e
Jared <jaredcmurphy@gmail.com>
2016-04-10 23:34:49 -0400

    adds material design to user select components

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -10,7 +10,7 @@ var UserDetails = require('./UserDetails');
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p>LOADING</p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.contentDiv}>
         <h1>Confirm Players</h1>
         <div className="col-sm-8 col-sm-offset-2">
             <UserDetailsWrapper header='Player One'>
@@ -22,13 +22,13 @@ function ConfirmBattle (props) {
         </div>
         <div className="col-sm-8 col-sm-offset-2">
             <div className="col-sm-12" style={styles.space}>
-              <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
+              <button type="button"className='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary' onClick={props.onInitiateBattle}>
                   Initiate Battle 
               </button>
             </div>
             <div className="col-sm-12" style={styles.space}>
               <Link to="/playerOne">
-                <button type="button" className="btn btn-lg btn-danger">
+                <button type="button" className='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent'> 
                   Reselect Players 
                 </button>
               </Link>
--- app/components/Home.js
+++ app/components/Home.js
@@ -2,6 +2,8 @@ var React = require('react');
 var center = require('../styles').center;
 var marginZero = require('../styles').marginZero;
 var inlineBlock = require('../styles').inlineBlock;
+var transparentBg = require('../styles').transparentBg;
+var contentDiv = require('../styles').contentDiv;
 var greyBg = require('../styles').greyBg;
 var title = require('../styles').title;
 var ReactRouter = require('react-router');
@@ -10,8 +12,8 @@ var Link = ReactRouter.Link;
 var Home = React.createClass({
   render: function() {
     return (
-      <div style={greyBg}> 
-        <h1 style={title} >Open Source Champion</h1>
+      <div style={contentDiv} className='jumbotron col-sm-12 text-center'>
+        <h1> Open Source Champion</h1>
         <p style={center}>
           What even is a jQuery?
         </p>

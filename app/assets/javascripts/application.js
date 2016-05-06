afb22483c140407ce53c45dfcbe05f540c0a8d9c
Jared <jaredcmurphy@gmail.com>
2016-05-06 17:32:24 -0400

    ajax now grabs data for each user and renders on page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,54 @@ $(document).ready(function() {
         var playerTwo = splitUrl[5];
         console.log('player one: ' + playerOne);
         console.log('player two: ' + playerTwo);
-           
 
+       
+        $.ajax({
+            url: 'https://api.github.com/users/' + playerOne,
+            type: 'GET',
+            data: {
+                format: 'json'
+            },
+            error: function() {
+              // error message
+            },
+            success: function(data) {
+                console.log(data)
+                 function score(data) {
+                     var followers = data.followers
+                     var publicRepos = data.public_repos
+                     return followers + publicRepos;
+                }
+                $('.p_one_location').append("location: " + data.location);
+                $('.p_one_img').attr('src', data.avatar_url);
+                var score = score(data);
+                $('.p_one_score').append("Score: " + score);
+            }
+         }); // ends player one ajax call
+
+        $.ajax({
+            url: 'https://api.github.com/users/' + playerTwo,
+            type: 'GET',
+            data: {
+                format: 'json'
+            },
+            error: function() {
+              // error message
+            },
+            success: function(data) {
+                console.log(data)
+                 function score(data) {
+                     var followers = data.followers
+                     var publicRepos = data.public_repos
+                     return followers + publicRepos;
+                }
+                $('.p_two_location').append("location: " + data.location);
+                $('.p_two_img').attr('src', data.avatar_url);
+                var score = score(data);
+                $('.p_two_score').append("Score: " + score);
+
+            }
+         }); // ends player one ajax call
 
         
     } else {

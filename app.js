b46877bafe437d1f079c91eabbc0d142df9caf6d
Jared <jaredcmurphy@gmail.com>
2016-08-10 19:06:31 -0400

    update boilerplate

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(session({
 app.use(flash());
 
 app.listen(3000, function () {
-  console.log('Auth Demo App Online!');
+  console.log('Open Source Champ Online! on 3000');
 });
 
 app.use(function(err, req, res, next){

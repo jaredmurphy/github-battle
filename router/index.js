3e9f55c4ad0eb4c0119658cd1bbbe9438bced4a7
Jared <jaredcmurphy@gmail.com>
2016-08-11 19:39:22 -0400

    adds battle page

--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
 module.exports = function(app){
   app.use('/', require('./routes/home'));
   app.use('/about', require('./routes/about'));
+  app.use('/battle', require('./routes/battle'));
   //app.use('/users', require('./routes/users'));
   //app.use('/sessions', require('./routes/sessions'));
 };
--- router/routes/battle.js
+++ router/routes/battle.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', function(req, res) {
+   res.render('battle/new');
+});
+
+module.exports = router;

773442b1c1b0b2dc4bf019ac3d3a4568fef37520
Jared <jaredcmurphy@gmail.com>
2016-08-11 19:11:13 -0400

    add tabs

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,13 @@ $(document).ready(function() {
     // activates collapsable menu on click
     $(".button-collapse").sideNav();
 
-    // // make menu links active on click
-    // $('.navbar_item').click(function() {
-    //   console.log('click')
-    //   $('.navbar_item').removeClass('active');
-    //   $(this).addClass('active');
-    // });
-
     // images carousel
     $('.carousel').carousel();
     setInterval(function() {
       $('.carousel').carousel('next');
     }, 1800);
+
+    // about page tabs
+    $('ul.tabs').tabs();
 
 }); // ends doc.ready

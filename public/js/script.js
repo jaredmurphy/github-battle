cdc70c82ed277029e3cbb7f24a0f4db9c37f23e4
Jared <jaredcmurphy@gmail.com>
2016-08-11 21:27:21 -0400

    adds input typing to github

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+    ///////////////////////////////////
+    ////// materialize functions //////
+    ///////////////////////////////////
+
     // activates collapsable menu on click
     $(".button-collapse").sideNav();
 
@@ -10,5 +15,31 @@ $(document).ready(function() {
 
     // about page tabs
     $('ul.tabs').tabs();
+
+    ///////////////////////////////////
+    // github username api functions //
+    ///////////////////////////////////
+
+    // ajax call to github
+    var getGithubUser = function() {
+      $.ajax({
+        type: "GET",
+        url: "https://api.github.com/users/" + $('#player_one_input').val(),
+        success: function(data){
+          $('#player_one_card').append('<img class="select_user" src=' + data.avatar_url + '/>');
+          $('#player_one_button').removeClass('disabled');
+          $('#player_one_button').addClass('waves-effect waves-light');
+          console.log(data);
+        }
+      });
+    };
+
+    var timer;
+
+    $('#player_one_input').on('keyup', function(e) {
+      clearTimeout(timer);
+      timer = setTimeout(getGithubUser, 1000);
+    });
+
 
 }); // ends doc.ready

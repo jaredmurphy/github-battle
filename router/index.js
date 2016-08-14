017098ff06bd004ca3a1e9333d643ecb6bf86b78
Jared <jaredcmurphy@gmail.com>
2016-08-14 04:32:50 -0400

    change name of leaderboard module

--- router/index.js
+++ router/index.js
@@ -1,11 +1,8 @@
 module.exports = function(app){
   app.use('/', require('./routes/home'));
   app.use('/about', require('./routes/about'));
-  app.use('/leaderboard', require('./routes/leaderBoard'));
+  app.use('/leaderboard', require('./routes/leaderboard.js'));
   app.use('/battle', require('./routes/battle'));
-  //app.use('/battle/:id', require('./routes/battle_show'));
   app.use('/battle/new', require('./routes/battle_new'));
   app.use('/create_or_update_githubUsers', require('./routes/githubUsers'));
-  //app.use('/users', require('./routes/users'));
-  //app.use('/sessions', require('./routes/sessions'));
 };

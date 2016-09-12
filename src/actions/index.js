eb736748642316d72f04a07a18b5d3e1d2d071eb
Jared <jaredcmurphy@gmail.com>
2016-09-11 23:54:37 -0400

    axios talks to localhost api

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 const GITHUB_ID = process.env["GITHUB_ID"];
 const GITHUB_SECRET = process.env["GITHUB_SECRET"];
 
-const RootUrl = "localhost:3001/api/v1/"
+const RootUrl = "http://localhost:3001/api/v1/"
 export const FETCH_PLAYER_ONE = "FETCH_PLAYER_ONE";
 export const FETCH_PLAYER_TWO = "FETCH_PLAYER_TWO";
 
 
 export function fetchPlayer(username, player){
   //const url = `${RootUrl}${username}?client_id=${GITHUB_ID}&client_secret=${GITHUB_SECRET}`;
-  const url = `http://admin:secret@${RootUrl}/players/${username}`;
+  const url = `${RootUrl}players/${username}`;
 
   const request = axios.get(url);
   const TYPE = player === "playerOne" ? FETCH_PLAYER_ONE : FETCH_PLAYER_TWO;

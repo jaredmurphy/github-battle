e1eea8f98f835f17f12167682f2e75d820567c5c
Jared Murphy <jaredcmurphy@gmail.com>
2017-03-09 22:50:24 -0500

    refactor how reducers send data to redux store

--- src/reducers/reducer_loser.js
+++ src/reducers/reducer_loser.js
@@ -3,7 +3,7 @@ import { FETCH_LOSER } from '../actions/index';
 export default function(state = null, action) {
   switch(action.type) {
     case FETCH_LOSER:
-      return { ...state, loser: action.payload.data  };
+      return { ...state, ...action.payload.data  };
     default:
       return state;
   }
